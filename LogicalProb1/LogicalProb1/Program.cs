63e8eea02302e87d96b035d1ba48988026d1e403
faruqesay2 <faruqesay786@gmail.com>
2022-10-26 15:49:45 +0530

    LogicalProbs1

---
+++ LogicalProb1/LogicalProb1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class GFG
+{
+
+
+    static void findSeries(int num,
+                int first, int sec)
+    {
+
+      
+        while (counter < num - 2)
+        {
+            sum = first + sec;
+            Console.Write(sum + " ");
+            first = sec;
+            counter++;
+        }
+    }
+
+  
+    public static void Main()
+    {
+        int n = 5, first = 2, sec = 4;
+
+        findSeries(n, first, sec);
+    }
+}
+
+// By FARUQESAY
+
